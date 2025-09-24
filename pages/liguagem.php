8b411eef44f680f90288c623557dbb5bf59e8472
YAGGO ROSA DE ALMEIDA SILVA <rosayaggo2@gmail.com>
2025-09-24 17:28:38 -0300

    git inicial

---
+++ pages/liguagem.php
@@ -0,0 +1,38 @@
+<?php
+$mensagem = "";
+$lingua = filter_input(INPUT_GET, 'lingua', FILTER_VALIDATE_INT);
+
+if (isset($lingua) == false ){
+    $mensagem = "lingua invalida";
+} else {
+    $url = "https://restcountries.com/v3.1/lang/{$lingua}/json/";
+    
+    $option = ["http" => ["method" => "GET", "header" => "Content-Type: application/json"]];
+
+    $context = stream_context_create($option);
+    $response = file_get_contents($url, false, $context);
+    
+    if ($response == false) {
+        $mensagem = "Erro ao acessar a API.";
+    } else { 
+        $dados = json_decode($response, true); 
+
+        if (isset($dados["erro"]) == true) {
+            $mensagem = "língua não encontrada.";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title> Línguas </title>
+</head>
+<body>
+    <div id=""
+</body>
+</html>
