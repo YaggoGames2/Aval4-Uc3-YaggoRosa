c200564afd89374b8620f8ebed46b42eb083e9ae
unknown <rosayaggo2@gmail.com>
2025-09-28 22:17:48 -0300

    finalização da atividade

--- pages/liguagem.php
+++ pages/liguagem.php
@@ -1,43 +1,59 @@
 <?php
 $mensagem = "";
-$lingua = filter_input(INPUT_GET, 'lingua', FILTER_SANITIZE_STRING);
-
-if (isset($lingua) == false ){
-    $mensagem = "lingua invalida.   ";
+$lingua = filter_input(INPUT_GET, 'linguaPais', FILTER_SANITIZE_SPECIAL_CHARS);
+ 
+if (isset($lingua) === false) {
+    $mensagem = "lingua invalida. ";
 } else {
     $url = "https://restcountries.com/v3.1/lang/{$lingua}";
-    
-    $option = ["http" => ["method" => "GET", "header" => "Content-Type: application/json"]];
-
+ 
+    $option = [
+        "http" => [
+            "method" => "GET",
+            "header" => "Content-Type: application/json"
+        ]
+    ];
+ 
     $context = stream_context_create($option);
     $response = file_get_contents($url, false, $context);
-    
+ 
     if ($response === false) {
         $mensagem = "Erro ao acessar a API.";
-    } else { 
-        $dados = json_decode($response, true); 
-        
-        if (isset($dados["erro"]) == false) {
-            $mensagem = "Nenhum país encontrado para a lingua '$lingua";
-        } else {
-            foreach ($dados as $dado) {
-               $mensagem .= "- " . $dado["name"]["common"] ."<br>";
-            }
+    } else {
+        $dados = json_decode($response, true);
+ 
+        if (isset($dados['status'])) {
+            $mensagem = "Nenhum país encontrado para a língua.";
+            $dados = [];
         }
-        
     }
 }
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="pt-br">
+ 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title> Línguas </title>
 </head>
+ 
 <body>
-   <div><?= $mensagem ?></div>
+<h1> Países: </h1>
+    <div id="cep-buscado">
+        <span id="error"><?= $mensagem ?></span>
+ 
+        <?php if (!empty($dados)): ?>
+            <?php foreach ($dados as $paises): ?>
+ 
+                <div>
+                    <input type="text" value="<?= $paises['name']['common'] ?? 'País não encontrado.' ?>" disabled>
+                </div>
+ 
+            <?php endforeach; ?>
+        <?php endif; ?>
 </body>
+ 
 </html>
